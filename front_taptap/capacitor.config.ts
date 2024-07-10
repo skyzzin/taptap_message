008211d5822a03af107fa2fc92ab6066d1e03acd
skyzzin <douglaspe982@gmail.com>
2024-07-09 23:40:18 -0300

    initial commite

---
+++ front_taptap/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'front_taptap',
+  webDir: 'dist'
+};
+
+export default config;
